4ee2a49aab8ad40303f72dc381385f50b83bb7ce
JB Work Laptop <208363846+Honour-Code-Web@users.noreply.github.com>
2026-03-22 20:04:17 +0000

    initial commit and setup

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/', (req, res) => res.send("index"));
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Express app listening on port ${PORT}`);
+});
